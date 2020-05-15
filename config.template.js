735cf693559af841a24e165ec65bea769e92e175
HarmoGlace <harmoglacehelp@gmail.com>
2020-05-15 12:53:25 +0200

    Initialization

---
+++ config.template.js
@@ -0,0 +1,7 @@
+const config = {
+    owners: [], // array of string, IDs of owners
+    serverId: '', // string, server id
+    token: '' // string, bot token
+}
+
+module.exports = config;
---
+++ struct/Client.js
@@ -0,0 +1,53 @@
+const { AkairoClient } = require('discord-akairo');
+const config = require('../config');
+
+class Client extends AkairoClient {
+
+    constructor() {
+        super({
+            ownerID: config.owners
+        }, {
+            partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER']
+        });
+
+        this.config = config;
+
+        this.commandHandler = new Handler(this, {
+            directory: './commands/',
+            prefix: "!",
+            allowMention: true
+        });
+
+        this.inhibitorHandler = new InhibitorHandler(this, {
+            directory: './inhibitors/'
+        });
+
+        this.listenerHandler = new ListenerHandler(this, {
+            directory: './events/'
+        });
+
+        this.listenerHandler.setEmitters({
+            commandHandler: this.commandHandler,
+            inhibitorHandler: this.inhibitorHandler,
+            listenerHandler: this.listenerHandler
+        });
+
+        this.commandHandler.useListenerHandler(this.listenerHandler);
+        this.listenerHandler.loadAll();
+
+        this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
+        this.inhibitorHandler.loadAll();
+
+        this.commandHandler.loadAll();
+
+    }
+
+    get server() {
+        return this.guilds.cache.get(config.serverId);
+    }
+
+
+
+}
+
+module.exports = Client;
